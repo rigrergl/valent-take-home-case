ac7d3d10c7e3d009847ced5e19997778e816c50d
rigrergl <rigrergl@gmail.com>
2023-03-27 21:46:31 -0500

    modified python script to add testing

--- compute_added_elements.py
+++ compute_added_elements.py
@@ -1,29 +1,44 @@
 from pulp import *
 
-q = [0.1479, 0.02, 0.005, 0.0001, 0.01, 0.0003, 0.0002, 0.006, 0.0005, 0.81]
-p = [0.17, 0.12, 0.025, 0.0008, 0.02, 0.0005, 0.0003, 0.0075, 0.001, 0.6549]
-w = 100
 
-# Create the optimization problem
-prob = LpProblem("Minimize_Y", LpMinimize)
+def compute_added_elements(q, p, w):
+    # Create the optimization problem
+    prob = LpProblem("Minimize_Y", LpMinimize)
 
-# Define the decision variables and their lower bounds
-x = [pulp.LpVariable(f"x{i}", lowBound=0) for i in range(1, 11)]
-y = pulp.LpVariable("y", lowBound=100)
+    # Define the decision variables and their lower bounds
+    x = [pulp.LpVariable(f"x{i}", lowBound=0) for i in range(1, 11)]
+    y = pulp.LpVariable("y", lowBound=100)
 
-# Add the objective function
-prob += y, "Objective Function"
+    # Add the objective function
+    prob += y, "Objective Function"
 
-# Define the constraints and add them to the problem
-for i in range(10):
-    prob += (q[i]*w + x[i]) == y*p[i], f"Constraint {i+1}"
+    # Define the constraints and add them to the problem
+    for i in range(10):
+        prob += (q[i]*w + x[i]) == y*p[i], f"Constraint {i+1}"
 
-prob += y == w + lpSum(x), "New Constraint"
+    prob += y == w + lpSum(x), "New Constraint"
 
-# Solve the problem
-prob.solve()
+    # Solve the problem
+    prob.solve()
 
-# Print the results
-print("Status:", LpStatus[prob.status])
-print("Optimal value of y:", value(prob.objective))
-print("Optimal values of x:", [value(x[i]) for i in range(0, 10)])
+    # Print the results
+    print("Status:", LpStatus[prob.status])
+    print("Optimal value of y:", value(prob.objective))
+    print("Optimal values of x:", [value(x[i]) for i in range(0, 10)])
+
+    return value(prob.objective), x  # minimum final weight, quantities of each element to be added
+
+
+def test():
+    q = [0.1479, 0.02, 0.005, 0.0001, 0.01, 0.0003, 0.0002, 0.006, 0.0005, 0.81]
+    p = [0.17, 0.12, 0.025, 0.0008, 0.02, 0.0005, 0.0003, 0.0075, 0.001, 0.6549]
+    w = 100
+
+    optimal_y, optimal_x = compute_added_elements(q, p, w)
+
+    for i in range(len(q)):
+        assert ((q[i] * w + optimal_x[i]) / optimal_y) == p[i]
+
+
+if __name__ == "__main__":
+    test()
